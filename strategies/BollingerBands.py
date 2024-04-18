5cc8599055f6cda61fb3c71c821dae6be76b50b0
sktrinh12 <sktrinh12@gmail.com>
2024-04-18 13:00:52 -0400

    rtn df for main function, use of self variables to capture signal

--- strategies/BollingerBands.py
+++ strategies/BollingerBands.py
@@ -4,7 +4,7 @@
         - generalised docs in detail here : https://algobulls.github.io/pyalgotrading/strategies/strategy_guides/common_strategy_guide/
 """
 
-from .tools.common import Strategy, np, pd, Tuple
+from .tools.common import Strategy, np, pd
 from .tools.tools import position_sizing, symbol_type
 
 
@@ -18,14 +18,28 @@ class BollingerBandsBot(Strategy):
     }
 
     def initialize(self):
+        self.last_price = 0.0
+        self.last_upper_band = None
+        self.last_lower_band = None
         print(f"symbol: {self.parameters['symbol']}")
 
-    def calculate_bollinger_bands(self, prices: np.array) -> Tuple[np.array, np.array, np.array]:
+    def calculate_bollinger_bands(self, prices: np.array) -> pd.DataFrame:
         rolling_mean = prices.rolling(window=self.parameters['window']).mean()
         rolling_std = prices.rolling(window=self.parameters['window']).std()
         upper_band = rolling_mean + (rolling_std * self.parameters['num_std_dev'])
         lower_band = rolling_mean - (rolling_std * self.parameters['num_std_dev'])
-        return rolling_mean, upper_band, lower_band
+
+        data = pd.DataFrame({'Price': prices,
+                             'RollingMean': rolling_mean,
+                             'RollingStd': rolling_std,
+                             'UpperBand': upper_band,
+                             'LowerBand': lower_band
+                            })
+        self.last_price = data['Price'].iloc[-1]
+
+        self.last_upper_band = data['UpperBand'].iloc[-1]
+        self.last_lower_band = data['LowerBand'].iloc[-1]
+        return data
 
 
     def on_trading_iteration(self):
@@ -35,26 +49,22 @@ class BollingerBandsBot(Strategy):
         if len(prices) < self.parameters['window']:
             return
 
-        rolling_mean, upper_band, lower_band = self.calculate_bollinger_bands(prices)
-
-        last_price = prices.iloc[-1]
-        last_upper_band = upper_band.iloc[-1]
-        last_lower_band = lower_band.iloc[-1]
+        data = self.calculate_bollinger_bands(prices)
 
         cash = self.get_cash()
-        quantity = position_sizing(cash, last_price, self.parameters['cash_at_risk'])
-        print(f"Last Price: {last_price}, Quantity: {quantity}, UB: {last_upper_band}, LB: {last_lower_band}")
+        quantity = position_sizing(cash, self.last_price, self.parameters['cash_at_risk'])
+        print(f"Last Price: {self.last_price}, Quantity: {quantity}, UB: {self.last_upper_band}, LB: {self.last_lower_band}")
 
-        if np.isnan(last_upper_band) or np.isnan(last_lower_band) or quantity <= 0:
+        if self.last_upper_band is None or self.last_lower_band is None or quantity <= 0:
             return
 
-        if last_price > last_upper_band:
+        if self.last_price > self.last_upper_band:
             pos = self.get_position(self.parameters['symbol'])
             if pos is not None:
                 self.sell_all()
-        elif (last_price < last_lower_band) and (cash > 0) and (quantity > 0):
-            take_profit_price = last_price * (1 + self.parameters['risk_tolerance'])
-            stop_loss_price = last_price * (1 - self.parameters['risk_tolerance'])
+        elif (self.last_price < self.last_lower_band) and (cash > 0) and (quantity > 0):
+            take_profit_price = self.last_price * (1 + self.parameters['risk_tolerance'])
+            stop_loss_price = self.last_price * (1 - self.parameters['risk_tolerance'])
             order = self.create_order(
                     self.parameters['symbol'],
                     quantity,
