902b0785068240d28239279ab55d615baeba8ed4
sktrinh12 <sktrinh12@gmail.com>
2024-04-18 13:00:35 -0400

    rtn df for main function, use of self variables to capture signal

--- strategies/AroonCrossover.py
+++ strategies/AroonCrossover.py
@@ -11,10 +11,12 @@ class AroonCrossover(Strategy):
     }
 
     def initialize(self):
+        self.signal = None
+        self.last_price = 0.0
         self.sleeptime = "1D"
         print(f"symbol: {self.parameters['symbol']}")
 
-    def get_aroon_signal(self, prices: np.array):
+    def get_aroon_signal(self, prices: np.array) -> pd.DataFrame:
         high_max_idx = prices.rolling(window=self.parameters['window']).apply(lambda x: x.argmax(), raw=True)
         low_min_idx = prices.rolling(window=self.parameters['window']).apply(lambda x: x.argmin(), raw=True)
 
@@ -23,30 +25,33 @@ class AroonCrossover(Strategy):
 
         aroon_signal = np.where(aroon_up > aroon_down, "BUY", np.where(aroon_up < aroon_down, "SELL", "HOLD"))
 
-        return aroon_signal
+        data = pd.DataFrame({'Price': prices, 'Signal': aroon_signal})
+        self.last_price = data['Price'].iloc[-1]
+        self.signal = data['Signal'].iloc[-1]
+
+        return data
 
     def on_trading_iteration(self):
         bars = self.get_historical_prices(self.parameters['symbol'], self.parameters['window'], "day")
         prices = bars.df['close']
-        signal = self.get_aroon_signal(prices)
-        print(f"signal: {signal[-1]}")
+        data = self.get_aroon_signal(prices)
+        print(f"signal: {self.signal}")
 
-        last_price = prices.iloc[-1]
         cash = self.get_cash()
-        quantity = position_sizing(cash, last_price, self.parameters['cash_at_risk'])
-        print(f"Last Price: {last_price}, Quantity: {quantity}")
+        quantity = position_sizing(cash, self.last_price, self.parameters['cash_at_risk'])
+        print(f"Last Price: {self.last_price}, Quantity: {quantity}")
 
-        if signal[-1] == 'BUY':
+        if self.signal == 'BUY':
             if cash > 0 and quantity > 0:
                 # Calculate take-profit and stop-loss prices based on risk tolerance
-                take_profit_price = last_price * (1 + self.parameters['risk_tolerance'])
-                stop_loss_price = last_price * (1 - self.parameters['risk_tolerance'])
+                take_profit_price = self.last_price * (1 + self.parameters['risk_tolerance'])
+                stop_loss_price = self.last_price * (1 - self.parameters['risk_tolerance'])
 
                 # Create a bracket order with take-profit and stop-loss prices
                 order = self.create_order(
                     self.parameters['symbol'],
                     quantity,
-                    "buy",
+                    side="buy",
                     type="bracket",
                     take_profit_price=take_profit_price,
                     stop_loss_price=stop_loss_price
@@ -54,7 +59,7 @@ class AroonCrossover(Strategy):
                 self.submit_order(order)
             else:
                 print(f"Error: cash: {cash}, quantity: {quantity}")
-        elif signal[-1] == "SELL":
+        elif self.signal == "SELL":
             pos = self.get_position(self.parameters['symbol'])
             if pos is not None:
                 self.sell_all()
