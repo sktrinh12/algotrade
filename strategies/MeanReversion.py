3592db37e4096753e1636f9ae78dea1be52769f3
sktrinh12 <sktrinh12@gmail.com>
2024-04-23 14:53:37 -0400

    separate class

--- strategies/MeanReversion.py
+++ strategies/MeanReversion.py
@@ -1,5 +1,25 @@
 from .tools.common import Strategy, np, pd
-from .tools.tools import position_sizing
+from .tools.tools import position_sizing, set_vars
+
+
+class MeanReversionCalc():
+
+    def __init__(self, params):
+        self.window = params['window']
+        self.z_threshold = params['z_threshold']
+
+    def get_data(self, prices: np.array) -> pd.DataFrame:
+        rolling_mean = prices.rolling(window=self.window).mean()
+        rolling_std = prices.rolling(window=self.window).std()
+
+        # Calculate the Z-score
+        z_score = (prices - rolling_mean) / rolling_std
+
+        # Define the trading signals
+        signal = np.where(z_score < -self.z_threshold, "BUY", np.where(z_score > self.z_threshold, "SELL", "HOLD"))
+
+        return pd.DataFrame({'Price': prices, 'Signal': signal})
+
 
 class MeanReversion(Strategy):
 
@@ -8,53 +28,40 @@ class MeanReversion(Strategy):
         "window": 0,
         "cash_at_risk": 0.0,
         "risk_tolerance": 0.0,
+        "z_threshold": 1.5
     }
 
     def initialize(self):
-        self.z_threshold = 1.5
         self.sleeptime = "1D"
-        self.signal = None
-        self.last_price = 0.0
-        print(f"symbol: {self.parameters['symbol']}")
-
-
-    def get_mean_reversion_signal(self, prices: np.array) -> pd.DataFrame:
-        rolling_mean = prices.rolling(window=self.parameters['window']).mean()
-        rolling_std = prices.rolling(window=self.parameters['window']).std()
-
-        # Calculate the Z-score
-        z_score = (prices - rolling_mean) / rolling_std
-
-        # Define the trading signals
-        signal = np.where(z_score < -self.z_threshold, "BUY", np.where(z_score > self.z_threshold, "SELL", "HOLD"))
-
-        data = pd.DataFrame({'Price': prices, 'Signal': signal})
-        self.last_price = data['Price'].iloc[-1]
-        self.signal = data['Signal'].iloc[-1]
-        return data
-
+        self.strategy = MeanReversionCalc(
+            params = {
+                'window': self.parameters['window'],
+                'z_threshold': self.parameters['z_threshold']
+            }
+        )
 
     def on_trading_iteration(self):
         bars = self.get_historical_prices(self.parameters['symbol'], self.parameters['window'], "day")
         prices = bars.df['close']
-        signal = self.get_mean_reversion_signal(prices)
-        print(f"signal: {self.signal}")
+        data = self.strategy.get_data(prices)
+        signal, last_price = set_vars(data, 'Signal')
+        print(f"{'':<4}{signal}")
 
         cash = self.get_cash()
-        quantity = position_sizing(cash, self.last_price, self.parameters['cash_at_risk'])
-        print(f"Last Price: {self.last_price}, Quantity: {quantity}")
+        quantity = position_sizing(cash, last_price, self.parameters['cash_at_risk'])
+        print(f"Last Price: {last_price}, Quantity: {quantity}")
 
-        if self.signal == 'BUY':
+        if signal == 'BUY':
             if cash > 0 and quantity > 0:
                 # Calculate take-profit and stop-loss prices based on risk tolerance
-                take_profit_price = self.last_price * (1 + self.parameters['risk_tolerance'])
-                stop_loss_price = self.last_price * (1 - self.parameters['risk_tolerance'])
+                take_profit_price = last_price * (1 + self.parameters['risk_tolerance'])
+                stop_loss_price = last_price * (1 - self.parameters['risk_tolerance'])
 
                 # Create a bracket order with take-profit and stop-loss prices
                 order = self.create_order(
                     self.parameters['symbol'],
                     quantity,
-                    "buy",
+                    side="buy",
                     type="bracket",
                     take_profit_price=take_profit_price,
                     stop_loss_price=stop_loss_price
@@ -62,7 +69,7 @@ class MeanReversion(Strategy):
                 self.submit_order(order)
             else:
                 print(f"Error: cash: {cash}, quantity: {quantity}")
-        elif self.signal == "SELL":
+        elif signal == "SELL":
             pos = self.get_position(self.parameters['symbol'])
             if pos is not None:
                 self.sell_all()
