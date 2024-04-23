b4e32a588dcfbdbf55cfdb9b6e1194475d177204
sktrinh12 <sktrinh12@gmail.com>
2024-04-23 14:54:20 -0400

    separate class, use of np.where nest

--- strategies/RsiCrossover.py
+++ strategies/RsiCrossover.py
@@ -1,5 +1,40 @@
 from .tools.common import Strategy, Trader, np, pd
-from .tools.tools import position_sizing
+from .tools.tools import position_sizing, set_vars
+
+
+class RsiCrossoverCalc():
+
+    def __init__(self, params):
+        self.rsi_period = params['rsi_period']
+        self.upper_threshold = params['upper_threshold']
+        self.lower_threshold = params['lower_threshold']
+
+    def get_data(self, prices: np.array) -> pd.DataFrame:
+        data = pd.DataFrame({'Price': prices})
+        # Calculate the day-to-day price difference (change)
+        data['delta'] = data['Price'].diff(1)
+        # Keep positive changes as gains, replace negative changes with 0
+        # if the value in the delta is not > 0 then set to 0; fill the NA with 0s
+        data['gain'] = (data['delta'].where(data['delta'] > 0, 0)).fillna(0)
+        # Keep negative changes as losses (as positive values), replace positive changes with 0
+        data['loss'] = (-data['delta'].where(data['delta'] < 0, 0)).fillna(0)
+        data['ewm_gain'] = data['gain'].ewm(span=self.rsi_period, min_periods=self.rsi_period).mean()  # Calculate the average gain over the RSI period
+        data['ewm_loss'] = data['loss'].ewm(span=self.rsi_period, min_periods=self.rsi_period).mean()  # Calculate the average loss over the RSI period
+        data['rs'] = data['ewm_gain'] / data['ewm_loss']  # Compute the Relative Strength (RS) value
+        data['RSI'] = 100 - (100 / (1 + data['rs']))  # Calculate the Relative Strength Index (RSI) value
+        # When the RSI is above the upper threshold, it suggests that the market is overbought, and it may be a good time to sell.
+
+        # When the RSI is below the lower threshold, it suggests that the market is oversold, and it may be a good time to buy
+        data['Signal'] = np.where(
+            data['RSI'] > self.upper_threshold,
+            'SELL',
+            np.where(
+                data['RSI'] < self.lower_threshold,
+                'BUY',
+                'HOLD'
+            )
+        )
+        return data
 
 
 class RsiCrossover(Strategy):
@@ -7,62 +42,34 @@ class RsiCrossover(Strategy):
     parameters = {
         'symbol': '',
         "window": 0,
-        'rsi_period': 0,
-        'upper_threshold': 0.0,
-        'lower_threshold': 0.0,
         'risk_tolerance': 0.0,
         'cash_at_risk': 0.0,
+        'upper_threshold': 0,
+        'lower_threshold': 0,
     }
 
     def initialize(self):
-        self.signal = None
-        self.last_price = 0
         self.sleeptime = "1D"
-        self.df = None
-
-    def calculate_rsi(self, prices: np.array) -> pd.DataFrame:
-        # Calculate the day-to-day price difference (change)
-        delta = prices.diff()
-        # Keep positive changes as gains, replace negative changes with 0
-        # if the value in the delta is not > 0 then set to 0; fill the NA with 0s
-        gain = (delta.where(delta > 0, 0)).fillna(0)
-        # Keep negative changes as losses (as positive values), replace positive changes with 0
-        loss = (-delta.where(delta < 0, 0)).fillna(0)
-        avg_gain = gain.rolling(self.parameters['rsi_period']).mean()  # Calculate the average gain over the RSI period
-        avg_loss = loss.rolling(self.parameters['rsi_period']).mean()  # Calculate the average loss over the RSI period
-        rs = avg_gain / avg_loss  # Compute the Relative Strength (RS) value
-        data = pd.DataFrame({'Price': prices,
-                             'Gain': gain,
-                             'Loss': loss,
-                             'AvgGain': avg_gain,
-                             'AvgLoss': avg_loss,
-                             'RS': rs
-                            })
-        data['RSI'] = 100 - (100 / (1 + rs))  # Calculate the Relative Strength Index (RSI) value
-        return data
-
-    def gen_rsi_signal(self, rsi_data: pd.DataFrame) -> pd.DataFrame:
-        rsi_data['Signal'] = 'HOLD'
-        # When the RSI is above the upper threshold, it suggests that the market is overbought, and it may be a good time to sell.
-        rsi_data['Signal'] = np.where(rsi_data['RSI'] > self.parameters['upper_threshold'], 'SELL', rsi_data['Signal'])
-        # When the RSI is below the lower threshold, it suggests that the market is oversold, and it may be a good time to buy
-        rsi_data['Signal'] = np.where(rsi_data['RSI'] < self.parameters['lower_threshold'], 'BUY', rsi_data['Signal'])
-        return rsi_data
+        self.strategy = RsiCrossoverCalc(
+            params = {
+                'rsi_period': self.parameters['short_window'],
+                'upper_threshold': self.parameters['upper_threshold'],
+                'lower_threshold': self.parameters['lower_threshold'],
+            }
+        )
 
     def on_trading_iteration(self):
         bars = self.get_historical_prices(self.parameters['symbol'], self.parameters['window'], "day")
-        data = self.calculate_rsi(bars.df['close'])
-        data = self.gen_rsi_signal(data)
-        self.signal = data['Signal'].iloc[-1]
-        self.last_price = bars.df['close'].iloc[-1]
-        print(f'signal: {self.signal}')
+        data = self.strategy.get_data(bars.df['close'])
+        signal, last_price = set_vars(data, 'Signal')
+        print(f"{'':<4}{signal}")
 
-        if self.signal == 'BUY':
+        if signal == 'BUY':
             cash = self.get_cash()
-            quantity = position_sizing(cash, self.last_price, self.parameters['cash_at_risk'])
+            quantity = position_sizing(cash, last_price, self.parameters['cash_at_risk'])
             if (cash > 0) and (quantity > 0):
-                take_profit_price = self.last_price * (1 + self.parameters['risk_tolerance'])
-                stop_loss_price = self.last_price * (1 - self.parameters['risk_tolerance'])
+                take_profit_price = last_price * (1 + self.parameters['risk_tolerance'])
+                stop_loss_price = last_price * (1 - self.parameters['risk_tolerance'])
                 order = self.create_order(
                         self.parameters['symbol'],
                         quantity,
@@ -73,7 +80,7 @@ class RsiCrossover(Strategy):
                 )
                 self.submit_order(order)
 
-        elif self.signal == 'SELL':
+        elif signal == 'SELL':
             pos = self.get_position(self.parameters['symbol'])
             if pos is not None:
                 self.sell_all()
